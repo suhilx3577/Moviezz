06036204ceac69a77ee80be406be65d93fd31668
suhilx3577 <suhilx3577@gmail.com>
2023-04-21 06:43:06 +0530

    Get API Call is Done and Data is Recieved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import  ReactDOM from "react-dom/client";
+import { fetchData } from "../utils/api";
 
 const App =() =>{
+
+    const [title, setTitle] = useState("Hello World")
+
+    useEffect(()=>{
+        apicall();
+    },[])
+    
+    const apicall =()  =>{
+        fetchData("/movie/popular")
+            .then((res)=> {
+                console.log(res.results[0].title)
+                setTitle(res?.results[0]?.title)
+            });
+    };
+    
     return(
-        <h1>Hello World</h1>
+        <h1>{title}</h1>
     )
 }
 
