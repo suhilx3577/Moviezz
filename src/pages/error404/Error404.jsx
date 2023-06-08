707c79c5e66c4699ad2844536ec44f13fc597c33
suhilx3577 <suhilx3577@gmail.com>
2023-06-08 23:48:43 +0530

    edited the Page Not Found

--- src/pages/error404/Error404.jsx
+++ src/pages/error404/Error404.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+
+import "./styles.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const Error404 = () => {
-  return (
-    <div>Error404</div>
-  )
-}
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
 export default Error404
